4e2f500de590e79d3e123673a12ef2b7164ecfef
Lindblad1999 <Lindblad1999@users.noreply.github.com>
2020-03-16 10:15:52 +0100

    eventhandlers added

--- Lommeregner/Lommeregner/MainWindow.xaml.cs
+++ Lommeregner/Lommeregner/MainWindow.xaml.cs
@@ -24,5 +24,58 @@ namespace Lommeregner
         {
             InitializeComponent();
         }
+
+        public List<double> nums = new List<double>();
+        public List<char> op = new List<char>();
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
