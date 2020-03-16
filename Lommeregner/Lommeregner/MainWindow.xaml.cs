18a37aa5665b53b6e70ac65b63e8aea8d5a2e71e
Lindblad1999 <Lindblad1999@users.noreply.github.com>
2020-03-16 10:49:39 +0100

    Button plus, og button equals, event handlers

--- Lommeregner/Lommeregner/MainWindow.xaml.cs
+++ Lommeregner/Lommeregner/MainWindow.xaml.cs
@@ -27,55 +27,91 @@ namespace Lommeregner
 
         public List<double> nums = new List<double>();
         public List<char> op = new List<char>();
+        public string currentString = String.Empty;
+        public double result = 0;
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-
+            createNum('1');
         }
 
         private void btn2_Click(object sender, RoutedEventArgs e)
         {
-
+            createNum('2');
         }
 
         private void btn3_Click(object sender, RoutedEventArgs e)
         {
-
+            createNum('3');
         }
 
         private void btn4_Click(object sender, RoutedEventArgs e)
         {
-
+            createNum('4');
         }
 
         private void btn5_Click(object sender, RoutedEventArgs e)
         {
-
+            createNum('5');
         }
 
         private void btn6_Click(object sender, RoutedEventArgs e)
         {
-
+            createNum('6');
         }
 
         private void btn7_Click(object sender, RoutedEventArgs e)
         {
-
+            createNum('7');
         }
 
         private void btn8_Click(object sender, RoutedEventArgs e)
         {
-
+            createNum('8');
         }
 
         private void btn9_Click(object sender, RoutedEventArgs e)
         {
-
+            createNum('9');
         }
 
         private void btn0_Click(object sender, RoutedEventArgs e)
         {
+            createNum('0');
+        }
 
+        private void createNum(char c)
+        {
+            currentString += c;
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            nums.Add(Convert.ToDouble(currentString));
+            for (int i = 0; i < op.Count; i++)
+            {
+                switch (op[i])
+                {
+                    case '+':
+                        result += nums[i];
+                        break;
+                    case '-':
+                        result -= nums[i];
+                        break;
+                    case '*':
+                        result *= nums[i];
+                        break;
+
+
+                }
+            }
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            nums.Add(Convert.ToDouble(currentString));
+            op.Add('+');
+            currentString = String.Empty;
         }
     }
 }
