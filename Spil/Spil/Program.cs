ad434f789a8ece168fd0d5d2ecd9b1c5a118a9cf
Lindblad1999 <Lindblad1999@users.noreply.github.com>
2020-03-17 10:33:35 +0100

    Spil project added, Console App
    Nedd pause from calculator

---
+++ Spil/Spil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
