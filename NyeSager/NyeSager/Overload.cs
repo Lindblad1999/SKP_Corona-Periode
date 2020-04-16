71b75897bb56c9593c00345f30fd31ec81a5a8a1
Lindblad1999 <Lindblad1999@users.noreply.github.com>
2020-04-16 14:58:09 +0200

    Lært overload og override, samt brug af abstraction og virtual

---
+++ NyeSager/NyeSager/Overload.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NyeSager
+{
+    public static class Overload
+    {
+        public static void met1(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("This is the first method");
+        }
+
+        public static void met1(string message, int num)
+        {
+            Console.WriteLine(message + num);
+            Console.WriteLine("This is the first method overload");
+        }
+
+        public static void met1(string message, int num, double num2)
+        {
+            Console.WriteLine(message + num + num2);
+            Console.WriteLine("This is the second method overload");
+        }
+
+        public static void Call()
+        {
+            met1("Hi");
+            met1("Hi", 5);
+            met1("Hi", 5, 6.56);
+        }
+    }
+}
---
+++ NyeSager/NyeSager/Override.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NyeSager
+{
+    public abstract class Override
+    {
+        public abstract void met1();
+    }
+
+    public class Override2 : Override
+    {
+        public override void met1()
+        {
+            throw new NotImplementedException();
+            //Code
+        }
+    }
+
+
+    public class Override3
+    {
+        public virtual void met1()
+        {
+            Console.WriteLine("Den gør noget");
+        }
+    }
+
+    public class Override4 : Override3
+    {
+        public override void met1()
+        {
+            Console.WriteLine("Nu gør den noget andet");
+        }
+    }
+}
